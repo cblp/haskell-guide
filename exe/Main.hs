b45f4f3c5025a0af0f48b57ebc4134c9f4588096
Yuriy Syrovetskiy <cblp@cblp.su>
2020-07-25 17:53:29 +0300

    Add rule for allPagesAsHtml

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,7 +1,15 @@
 import           RIO
 
-import           Development.Shake (shakeArgs, shakeOptions)
+import           Development.Shake (getDirectoryFiles, need, shakeArgs,
+                                    shakeOptions, want, (~>))
+import           Development.Shake.FilePath ((-<.>), (</>))
 
 main :: IO ()
-main = shakeArgs shakeOptions $ do
-  pure ()
+main =
+  shakeArgs shakeOptions $ do
+    want [allPagesAsHtml]
+    allPagesAsHtml ~> do
+      pages <- getDirectoryFiles "pages" ["//*.md"]
+      need ["_site" </> page -<.> "html" | page <- pages]
+  where
+    allPagesAsHtml = ".allPagesAsHtml"
